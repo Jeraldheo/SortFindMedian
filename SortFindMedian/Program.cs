245b501d5c11198ce00ca651d2f8b2d6ceb117fd
Jeraldheo <jeyson.marin@unah.hn>
2023-09-17 08:21:42 -0600

    Implemented and tested TwoDimensionToOneDimension

--- SortFindMedian/Program.cs
+++ SortFindMedian/Program.cs
@@ -1,10 +1,26 @@
 ﻿namespace SortFindMedian
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        public static int[] TwoDimensionToOneDimension(int[,] matrix)
+        {
+            int numRows = matrix.GetLength(0);
+            int numCols = matrix.GetLength(1); 
+            int[] array = new int[numRows*numCols];
+            for(int i = 0; i<numRows; i++)
+            {
+                for(int j = 0; j<numCols; j++)
+                {
+                    int oneDimensionalIndex = numRows*i + j;
+                    array[oneDimensionalIndex] = matrix[i, j];
+                }
+            }
+            return array;
         }
     }
 }
--- TestSortFindMedian/TestProgram.cs
+++ TestSortFindMedian/TestProgram.cs
@@ -1,11 +1,33 @@
+
+
+using SortFindMedian;
+
 namespace TestSortFindMedian
 {
     public class TestProgram
     {
-        [Fact]
-        public void Test1()
-        {
+        public static IEnumerable<object[]> TestDataTwoDemensionToOneDimension
+            => new[]
+            {
+                new object[]{new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                            new int[,]{ {0, 0, 0 },{0, 0, 0 },{0, 0, 0 } } },
 
+                new object[]{new int[]{17, 9, 36, 8, 7, 3, 15, 28, 57},
+                            new int[,]{ {17, 9, 36 },{8, 7, 3 },{15, 28, 57 } } },
+
+                new object[]{new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9},
+                            new int[,]{ { 1, 2, 3 },{ 4, 5, 6 },{ 7, 8, 9 } } }
+
+            };
+
+        [Theory]
+        [MemberData(nameof(TestDataTwoDemensionToOneDimension))]
+        public static void Test_TwoDemensionToOneDimension(int[] expectedArray, int[,] matrix)
+        {
+            int[] array = Program.TwoDimensionToOneDimension(matrix);
+            Assert.Equal(expectedArray, array);
         }
+
+
     }
 }
