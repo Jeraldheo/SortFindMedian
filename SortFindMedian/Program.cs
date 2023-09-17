d3907233a185b474a0bc817d985949c7ab5407b9
Jeraldheo <jeyson.marin@unah.hn>
2023-09-17 13:22:43 -0600

    Finished SortFindMedian

--- SortFindMedian/Program.cs
+++ SortFindMedian/Program.cs
@@ -1,17 +1,24 @@
-﻿namespace SortFindMedian
+﻿using System.Text;
+
+namespace SortFindMedian
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            double[,] matrix = new double[,] { { 17, 9, 36 }, { 8, 7, 3 }, { 15, 28, 57 } };
+            double[] array = TwoDimensionToOneDimension(matrix);
+            //Array.Sort(array); Time complexity O(nlogn) from standard library.
+            SortArray(array);
+            Console.WriteLine($"Elementos ordenados: {ArrayToString(array)}");
+            Console.WriteLine($"Mediana: {ComputeMedian(array)}");
         }
 
-        public static int[] TwoDimensionToOneDimension(int[,] matrix)
+        public static double[] TwoDimensionToOneDimension(double[,] matrix)
         {
             int numRows = matrix.GetLength(0);
             int numCols = matrix.GetLength(1); 
-            int[] array = new int[numRows*numCols];
+            double[] array = new double[numRows*numCols];
             for(int i = 0; i<numRows; i++)
             {
                 for(int j = 0; j<numCols; j++)
@@ -23,7 +30,7 @@
             return array;
         }
 
-        public static int GetIndexSmallestElement(int[] array, int arrayIndex)
+        public static int GetIndexSmallestElement(double[] array, int arrayIndex)
         {
             int indexSmallestElement = arrayIndex;
             int length = array.Length;
@@ -38,16 +45,48 @@
         }
 
         //Time complexity O(n^2)
-        public static void SortArray(int[] array)
+        public static void SortArray(double[] array)
         {
             int length = array.Length;
             for(int i = 0; i<length; i++)
             {
                 int indexCurrentSmallestElement = GetIndexSmallestElement(array, i);
-                int temporal = array[indexCurrentSmallestElement];
+                double temporal = array[indexCurrentSmallestElement];
                 array[indexCurrentSmallestElement] = array[i];
                 array[i] = temporal;
             }
+        }
+
+        public static double ComputeMedian(double[] sortedArray)
+        {
+            int numElements = sortedArray.Length;
+            double median = 0;
+            if(numElements%2==0)
+            {
+                int indexRightElement = numElements / 2;
+                int indexLeftElement = indexRightElement-1;
+                median = (sortedArray[indexLeftElement] + sortedArray[indexRightElement])/2;
+            }
+            else
+            {
+                int medianIndex  = numElements/2;
+                median = sortedArray[medianIndex];
+            }
+            return median;
+        }
+
+        private static string ArrayToString(double[] array)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int size = array.Length;
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                stringBuilder.Append(array[i] + ", ");
+            }
+
+            stringBuilder.Append(array[size - 1]);
+            return stringBuilder.ToString();
         }
 
     }
--- TestSortFindMedian/TestProgram.cs
+++ TestSortFindMedian/TestProgram.cs
@@ -9,40 +9,40 @@ namespace TestSortFindMedian
         public static IEnumerable<object[]> TestDataTwoDemensionToOneDimension
             => new[]
             {
-                new object[]{new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0},
-                            new int[,]{ {0, 0, 0 },{0, 0, 0 },{0, 0, 0 } } },
+                new object[]{new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                            new double[,]{ {0, 0, 0 },{0, 0, 0 },{0, 0, 0 } } },
 
-                new object[]{new int[]{17, 9, 36, 8, 7, 3, 15, 28, 57},
-                            new int[,]{ {17, 9, 36 },{8, 7, 3 },{15, 28, 57 } } },
+                new object[]{new double[]{17, 9, 36, 8, 7, 3, 15, 28, 57},
+                            new double[,]{ {17, 9, 36 },{8, 7, 3 },{15, 28, 57 } } },
 
-                new object[]{new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9},
-                            new int[,]{ { 1, 2, 3 },{ 4, 5, 6 },{ 7, 8, 9 } } }
+                new object[]{new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9},
+                            new double[,]{ { 1, 2, 3 },{ 4, 5, 6 },{ 7, 8, 9 } } }
 
             };
 
         [Theory]
         [MemberData(nameof(TestDataTwoDemensionToOneDimension))]
-        public static void Test_TwoDemensionToOneDimension(int[] expectedArray, int[,] matrix)
+        public static void Test_TwoDemensionToOneDimension(double[] expectedArray, double[,] matrix)
         {
-            int[] array = Program.TwoDimensionToOneDimension(matrix);
+            double[] array = Program.TwoDimensionToOneDimension(matrix);
             Assert.Equal(expectedArray, array);
         }
 
         public static IEnumerable<object[]> TestDataGetIndexSmallestElement
             => new[]
             {
-                new object[]{5, new int[] { 17, 9, 36, 8, 7, 3, 15, 28, 57 }, 0},
-                new object[]{0, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 0},
-                new object[]{8, new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }, 0},
-                new object[]{4, new int[] { 1, 2, 3, 4, 0, 6, 7, 8, 9 }, 0},
-                new object[]{3, new int[] { 2, 9, 4, 0, 7, 25, 3, 1, 19 }, 0},
-                new object[]{7, new int[] { 2, 9, 4, 0, 7, 25, 3, 1, 19 }, 4}
+                new object[]{5, new double[] { 17, 9, 36, 8, 7, 3, 15, 28, 57 }, 0},
+                new object[]{0, new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 0},
+                new object[]{8, new double[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }, 0},
+                new object[]{4, new double[] { 1, 2, 3, 4, 0, 6, 7, 8, 9 }, 0},
+                new object[]{3, new double[] { 2, 9, 4, 0, 7, 25, 3, 1, 19 }, 0},
+                new object[]{7, new double[] { 2, 9, 4, 0, 7, 25, 3, 1, 19 }, 4}
 
             };
 
         [Theory]
         [MemberData(nameof(TestDataGetIndexSmallestElement))]
-        public static void Test_GetIndexSmallestElement(int expectedIndex, int[] array, int arrayIndex)
+        public static void Test_GetIndexSmallestElement(int expectedIndex, double[] array, int arrayIndex)
         {
             int index = Program.GetIndexSmallestElement(array, arrayIndex);
             Assert.Equal(expectedIndex, index);
@@ -52,20 +52,40 @@ namespace TestSortFindMedian
         public static IEnumerable<object[]> TestDataSortArray
             => new[]
             {
-                new object[]{ new int[] { 3, 7, 8, 9, 15, 17, 28, 36, 57 }, new int[] { 17, 9, 36, 8, 7, 3, 15, 28, 57 }},
-                new object[]{ new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }},
-                new object[]{ new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }},
-                new object[]{ new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 }},
-                new object[]{ new int[] { 1, 1, 1, 1, 1, 2, 2, 2, 2 }, new int[] { 1, 2, 1, 2, 1, 2, 1, 2, 1 }},
-                new object[]{ new int[] {1}, new int[] {1} }
+                new object[]{ new double[] { 3, 7, 8, 9, 15, 17, 28, 36, 57 }, new double[] { 17, 9, 36, 8, 7, 3, 15, 28, 57 }},
+                new object[]{ new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }},
+                new object[]{ new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new double[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }},
+                new object[]{ new double[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 }, new double[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 }},
+                new object[]{ new double[] { 1, 1, 1, 1, 1, 2, 2, 2, 2 }, new double[] { 1, 2, 1, 2, 1, 2, 1, 2, 1 }},
+                new object[]{ new double[] {1}, new double[] {1} }
 
             };
         [Theory]
         [MemberData(nameof(TestDataSortArray))]
-        public static void Test_SortArray(int[] expectedSortedArray, int[] array)
+        public static void Test_SortArray(double[] expectedSortedArray, double[] array)
         {
             Program.SortArray(array);
             Assert.Equal(expectedSortedArray, array);
+        }
+
+        public static IEnumerable<object[]> TestDataComputeMedian
+            => new[]
+            {
+                new object[]{15, new double[] { 3, 7, 8, 9, 15, 17, 28, 36, 57 }},
+                new object[]{3, new double[] { 1, 2, 3, 4, 5 }},
+                new object[]{5, new double[] { 2, 3, 4, 6, 7, 8 }},
+                new object[]{2, new double[] { 2 }},
+                new object[]{2.5, new double[] { 2, 3 }},
+
+
+            };
+        [Theory]
+        [MemberData(nameof(TestDataComputeMedian))]
+        public static void Test_ComputeMedian(double expectedMedian, double[] sortedArray)
+        {
+            double median = Program.ComputeMedian(sortedArray);
+            Assert.Equal(expectedMedian, median);
+
         }
     }
 }
